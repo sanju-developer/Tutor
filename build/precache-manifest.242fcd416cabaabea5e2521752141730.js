bfe738453c93ca37c94b04dda01e9aafa057c606
sanju-developer <vivekrajoriya106@gmail.com>
2020-05-10 18:12:50 +0530

    revamp code

---
+++ build/precache-manifest.242fcd416cabaabea5e2521752141730.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35112e0807c09d34334c0d540e5b3b8",
+    "url": "/sanju-developer/Tutor/index.html"
+  },
+  {
+    "revision": "f991f486c05728cf7441",
+    "url": "/sanju-developer/Tutor/static/css/main.c690b984.chunk.css"
+  },
+  {
+    "revision": "2bba7c59c8b4bb9f6675",
+    "url": "/sanju-developer/Tutor/static/js/2.25f7c8fb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sanju-developer/Tutor/static/js/2.25f7c8fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f991f486c05728cf7441",
+    "url": "/sanju-developer/Tutor/static/js/main.d0fcf53a.chunk.js"
+  },
+  {
+    "revision": "cd732f5d3222909c5654",
+    "url": "/sanju-developer/Tutor/static/js/runtime-main.4b5986be.js"
+  },
+  {
+    "revision": "3ddcb5ae755b43e4c0e2da93817105b6",
+    "url": "/sanju-developer/Tutor/static/media/contact.3ddcb5ae.svg"
+  },
+  {
+    "revision": "e8768f4736889bcbc7f6ac4b4f89791b",
+    "url": "/sanju-developer/Tutor/static/media/security.e8768f47.svg"
+  },
+  {
+    "revision": "64467ece627bb5ecfd1d529c25ae39b2",
+    "url": "/sanju-developer/Tutor/static/media/signup.64467ece.svg"
+  },
+  {
+    "revision": "f644c898af31276ea4dbcd5450232c63",
+    "url": "/sanju-developer/Tutor/static/media/student-class.f644c898.svg"
+  },
+  {
+    "revision": "720695c21106393a102f591c77777135",
+    "url": "/sanju-developer/Tutor/static/media/support.720695c2.svg"
+  },
+  {
+    "revision": "89b0e2f53c2f0c616acbbe2289755af8",
+    "url": "/sanju-developer/Tutor/static/media/team.89b0e2f5.svg"
+  },
+  {
+    "revision": "ba63589468d4f322379219c66490744a",
+    "url": "/sanju-developer/Tutor/static/media/tutor-class.ba635894.svg"
+  }
+]);
